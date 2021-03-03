9e9ddbb82269294ca36375b73b800f61cb68a6d7
wildarte <wildarte10@gmail.com>
2021-03-03 18:58:19 -0300

    commit da primeira section da home

--- app/Views/site/home.php
+++ app/Views/site/home.php
@@ -1,5 +1,21 @@
 <?php include '../app/Views/header.php'; ?>
 
-<h3><?= $dados['tituloPagina']; ?></h3>
-<h5><?= $dados['descricao']; ?></h5>
+<section class="container">
+    <div class="row">
+        <div class="left-home">
+            <h2 class="left-home-text">Nova ferramenta para fazer redirecionamento de grupos de WhatsApp</h2>
+            <div class="row">
+                <div class="botao-align " style="margin: 0 auto;">
+                    <a href="<?=URL?>/public/" class="btn btn-success btn-lg">Experimentar</a>
+                </div>
+            </div>
+        </div>
+
+        <div class="right-home">
+            <img class="img-fluid" src="<?=URL?>/public/img/logo-2.png" alt="">
+        </div>
+    
+    </div>
+
+</section>
 
